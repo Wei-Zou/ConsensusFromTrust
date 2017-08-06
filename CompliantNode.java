45abb9776e2dad531b6876d3e1c4137fd59dc164
Wei-Zou <wz.weizou@gmail.com>
2017-08-06 16:42:17 -0700

    first submission

--- CompliantNode.java
+++ CompliantNode.java
@@ -1,26 +1,53 @@
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Set;
 
 /* CompliantNode refers to a node that follows the rules (not malicious)*/
 public class CompliantNode implements Node {
+    private static final int REPETITION_CRITERIA = 2;
+
+    private boolean[] followees;
+    private Set<Transaction> initTransactions;
+    private Set<Transaction> proposalTransactions = new HashSet<Transaction>();
+    private HashMap<Integer, Integer> txRepetition = new HashMap<Integer, Integer>();
 
     public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
         // IMPLEMENT THIS
     }
 
     public void setFollowees(boolean[] followees) {
-        // IMPLEMENT THIS
+        this.followees = followees;
     }
 
     public void setPendingTransaction(Set<Transaction> pendingTransactions) {
-        // IMPLEMENT THIS
+        initTransactions = pendingTransactions;
+        proposalTransactions.addAll(pendingTransactions);
+        for (Transaction tx: pendingTransactions) {
+            txRepetition.put(tx.id, 1);
+        }
     }
 
     public Set<Transaction> sendToFollowers() {
-        // IMPLEMENT THIS
+        return proposalTransactions;
     }
 
     public void receiveFromFollowees(Set<Candidate> candidates) {
-        // IMPLEMENT THIS
+        for (Candidate candidate: candidates) {
+            if (!followees[candidate.sender]) {
+                continue;
+            }
+
+            Transaction candidateTx = candidate.tx;
+            int txId = candidateTx.id;
+
+            if (txRepetition.containsKey(txId)) {
+                txRepetition.put(txId, txRepetition.get(txId) + 1);
+                if (txRepetition.get(txId) >= REPETITION_CRITERIA) {
+                    proposalTransactions.add(candidateTx);
+                }
+            } else {
+                txRepetition.put(txId, 1);
+            }
+        }
     }
 }
